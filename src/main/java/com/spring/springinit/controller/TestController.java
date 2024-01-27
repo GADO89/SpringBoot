18004d65e0a59be8a94620dd7a40021b6b55f420
GADO89 <gadomahmoud377@gmail.com>
2024-01-27 16:38:27 +0200

    Run Project in Console

--- src/main/java/com/spring/springinit/controller/TestController.java
+++ src/main/java/com/spring/springinit/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Run Project in Console
 @RestController
 @RequestMapping("/api")
 public class TestController {
